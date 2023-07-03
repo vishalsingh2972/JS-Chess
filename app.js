958e946571b3cd257ca5c87d7a2abae2b7abfd42
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-07-03 19:11:41 +0530

    board set

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const startPieces = [
     '', '', '', '', '', '', '', '',
     '', '', '', '', '', '', '', '',
     '', '', '', '', '', '', '', '',
-    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
-    rook, knight, bishop, king, queen, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn, 
+    rook, knight, bishop, queen, king, bishop, knight, rook,
 ];
 
 function createBoard(){
@@ -23,15 +23,24 @@ function createBoard(){
   
       square.setAttribute('square-id', index) //like document.createElement allows us to create html elements dynamically via js similarly .setAttribute helps us change the properties or basically make changes inside html elements via js //The setAttribute() method can be used to modify the attributes of any HTML element, whether it was created dynamically using document.createElement() or obtained from the DOM using methods like document.querySelector() //When I mention "obtained from the DOM," it means accessing HTML elements in JavaScript and treating them as objects
                                               //so here we have set a 'square-id' for each element as it loops thorugh the array and the value of that 'square-id' for each element is its index value
-      //square.classList.add('green');
-      const row = Math.floor((63 - index) / 8) + 1;
+
+      const row = Math.floor((63 - index) / 8) + 1; //index startion from 0 to 63
       if(row % 2 === 0){
         square.classList.add(index % 2 === 0 ? "beige" : "green");
       }
       else{
         square.classList.add(index % 2 === 0 ? "green" : "beige");
       }
+
+      if(index <= 15){
+        square.firstElementChild.firstElementChild.classList.add('black');//square class first child is piece class, piece class first child is svg
+      }
+
+      if(index >= 48){
+        square.firstElementChild.firstElementChild.classList.add('brown');
+      }
       gameBoard.append(square);//at each loop we are putting these small squares onto out gameBoard                           
-    });}
+    })
+}
 
     createBoard();
